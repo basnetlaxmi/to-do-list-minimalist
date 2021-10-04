47eebac8155db8e55485a3e8906999330e9a5309
basnetlaxmi <ddsanzel@gmail.com>
2021-10-04 20:23:48 +0545

    Add tasks.js file and create array of object

---
+++ src/js/tasks.js
@@ -0,0 +1,20 @@
+import '../style.css';
+
+const tasks = [
+  {
+    index: 1,
+    description: 'Play Music',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'Edit Video',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Play Game',
+    completed: false,
+  },
+];
+
