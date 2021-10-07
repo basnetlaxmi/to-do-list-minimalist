e4ac47e676e14cdcf25e6d04e69a7ca35027a209
basnetlaxmi <ddsanzel@gmail.com>
2021-10-07 23:24:55 +0545

    Add function of adding items

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -51,5 +51,43 @@ function displayTasks(taskArray = getTask) {
   const sortTasks = taskArray.sort((a, b) => a.index - b.index);
   list.innerHTML = sortTasks.map((t) => task(t)).join('');
   handleCheck();
+  // eslint-disable-next-line no-use-before-define
+  // editTask();
+  // show();
 }
+const updateIndex = (array) => array.map((el, i) => {
+  const newEl = el;
+  newEl.index = i + 1;
+  return newEl;
+});
+
+function addItem(taskName) {
+  let allTasks = getTask || tasks;
+  const newTask = { index: 0, description: taskName, completed: false };
+  allTasks.push(newTask);
+  allTasks = updateIndex(allTasks);
+  localStorage.setItem('taskList', JSON.stringify(allTasks));
+}
+
+const title = document.querySelector('.item');
+const enter = document.getElementById('enter');
+
+function handleAddTaskOnForm(e) {
+  e.preventDefault();
+  const taskName = title.value;
+  if (taskName.length > 0) {
+    addItem(taskName);
+    title.value = '';
+    displayTasks();
+  }
+}
+
+title.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleAddTaskOnForm(e);
+  }
+});
+
+enter.addEventListener('click', handleAddTaskOnForm);
+
 export { tasks, task, displayTasks };
